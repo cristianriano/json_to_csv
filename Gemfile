afa6d25ffe39abae2dc01f714bb710891159fd5c
Cristian Riaño <cristiancarm11@gmail.com>
2019-11-09 20:23:33 +0100

    Setup rspec

--- Gemfile
+++ Gemfile
@@ -6,5 +6,7 @@ gem "puma", "~> 4.3"
 gem "sinatra", "~> 2.0"
 
 group :test do
+  gem "rack-test", "~> 1.1"
+  gem "rspec", "~> 3.9"
   gem "pry", "~> 0.12"
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-ENV["APP_ENV"] = ENV["ENVIRONMENT"] ||= "development"
+ENV["RACK_ENV"] = ENV["APP_ENV"] = ENV["ENVIRONMENT"] ||= "development"
 
 require "rubygems"
 require "bundler"
